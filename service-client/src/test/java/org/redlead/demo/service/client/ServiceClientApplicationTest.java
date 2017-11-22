2102880d733b1ac4677eebd35d66a4f2eecda888
Sergey Ryzhkov <ryzhkovsv@gmail.com>
2017-11-22 17:08:59 +0300

    add simple tests

---
+++ service-client/src/test/java/org/redlead/demo/service/client/ServiceClientApplicationTest.java
@@ -0,0 +1,16 @@
+package org.redlead.demo.service.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceClientApplicationTest {
+
+    @Test
+    public void contextLoad() {
+        // nothing to do
+    }
+}
